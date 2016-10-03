3a73c0bd9407f55a995c19f817b5b65a7c337df0
Nicolas Angelard-Gontier <gontier.nicolas@yahoo.fr>
2016-10-03 17:12:41 -0400

    continuation of tutorials...

--- theano_tuto.py
+++ theano_tuto.py
@@ -24,7 +24,7 @@ print "a = b = [[0,1],[-1,-2]]. [a-b, abs(a-b), (a-b)^2] ="
 print super_function([[0, 1], [-1, -2]], [[0, 1], [-1, -2]])
 print "- - -"
 
-### Setting a Default Value for an Argument
+### Setting a Default Value for an Argument with theano.In()
 
 x, y, w = T.dscalars('x', 'y', 'w')
 z = (x+y)*w
@@ -110,6 +110,80 @@ random_variable_uniform.rng.set_value(random_generator, borrow=True)   # set bac
 semi_random_number_generator.seed(234)  # seed that will generate a random seed for each random variables
 print "- - -"
 
-###
+### Computing Gradients
 
+x = T.dscalar('x')
+y = x**2
+gy = T.grad(y, x)  # dy/dx = 2*x
+theano.pp(gy)  # pretty print
+
+f = theano.function([x], [gy])
+print "f = grad(x^2). f(4) =", f(4)  # 8.0
+print "- - -"
+
+### Computing manually the Jacobian of y w.r.t. x with theano.scan()
+# The term Jacobian designates the tensor comprising the first partial
+# derivatives of the output of a function with respect to its inputs.
+
+x = T.dvector('x')
+y = x**2
+J, updates = theano.scan(lambda i, y, x: T.grad(y[i], x), sequences=T.arange(y.shape[0]), non_sequences=[y, x])
+
+f = theano.function([x], [J], updates=updates)
+print "f = jacobian(x^2) = 2x. f([4,4]) ="; print f([4,4])  # array([[8.,0.],[0.,8.]])
+
+# Can also do it automatically
+
+J = theano.gradient.jacobian(y, x)
+f = theano.function([x], [J])
+print "f = jacobian(x^2) = 2x. f([4,4]) ="; print f([4,4])
+print "- - -"
+
+### Computing manually the Hessian of y w.r.t. x with T.grad() & theano.scan()
+# The term Hessian designates the matrix comprising the second order
+# partial derivative of a function with scalar output and vector input.
+
+x = T.dvector('x')
+y = x**2
+cost = y.sum()
+
+gy = T.grad(cost, x)
+H, updates = theano.scan(lambda i, gy, x: T.grad(gy[i], x), sequences=T.arange(gy.shape[0]), non_sequences=[gy, x])
+f = theano.function([x], [H], updates=updates)
+print "f = hessian(x^2) = grad(2x) = 2. f([4,4]) ="; print f([4,4])
+
+# Can also do it automatically
+
+H = theano.gradient.hessian(cost, x)
+f = theano.function([x], [H])
+print "f = hessian(x^2) = grad(2x) = 2. f([4,4]) ="; print f([4,4])
+print "- - -"
+
+### Jacobian times a Vector
+# The R operator is built to evaluate the product between a Jacobian and a vector, namely f'(x) * v.
+
+x = T.dvector('x')
+W = T.dmatrix('W')
+V = T.dmatrix('V')
+
+y = T.dot(x, W)
+JV = T.Rop(y, wrt=W, eval_points=V)
+f = theano.function([x, W, V], [JV])
+
+print "Jacobian * V =", f(x=[0, 1], W=[[1, 1], [1, 1]], V=[[2, 2], [2, 2]])  # [2, 2]
+print "- - -"
+
+### Vector times Jacobian
+# The L-operator would compute a row vector times the Jacobian, namely v * f'(x).
+
+x = T.dvector('x')
+W = T.dmatrix('W')
+V = T.dvector('V')
+
+y = T.dot(x, W)
+VJ = T.Lop(y, wrt=W, eval_points=V)
+f = theano.function([x, W, V], [VJ], on_unused_input='warn')  # W is not used to compute f()!
+
+print "V * Jacobian =", f(x=[0, 1], W=[[1, 1], [1, 1]], V=[2, 2])  # [[0,0],[2,2]]
+print "- - -"
 
